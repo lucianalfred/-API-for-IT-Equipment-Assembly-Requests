199d540e7d54e03245605d87c0081a971f4b8099
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 09:56:08 +0100

    component models

--- src/models/Component.js
+++ src/models/Component.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/database');
 const Component = sequelize.define('Component',{
     idComponent:{type:DataTypes.STRING, autoIncrement:true, allowNull:false},
     name:{type:DataTypes.STRING, allowNull:false},
+    price: {type:DataTypes.FLOAT,allowNull:false},
     description:{type:DataTypes.STRING, allowNull:false},
     quantity:{type:DataTypes.INTEGER, allowNull:false},
     requestComponentId:{type:DataTypes.INTEGER, allowNull:false},
