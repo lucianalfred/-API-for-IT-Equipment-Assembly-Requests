819127040bb0f0668bf34d74e5c5424cd92b9f63
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 09:13:06 +0100

    Request model

---
+++ src/models/Request.js
@@ -0,0 +1,9 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require('../config/database');
+
+const Request =  sequelize.define('Request',{
+    idRequest: {type:DataTypes.INTEGER, autoIncrement:true,allowNull:false},
+    status: {type:DataTypes.BOOLEAN,allowNull:false, defaultValue:false},
+    priority:{type:DataTypes.INTEGER,allowNull:false},
+    technicianId:{type:DatatTypes.INTEGER, allowNull:false},
+});
