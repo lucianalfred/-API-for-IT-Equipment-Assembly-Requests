d312a7b9015f0e0c5f0b009c86daba49369eb9c1
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 09:44:45 +0100

    Applicant config

--- src/models/Applicant.js
+++ src/models/Applicant.js
@@ -3,10 +3,9 @@ const sequelize = require('../config/database');
 
 const Applicant = sequelize.define('Applicant',{
     idApplicant: {type:DataTypes.INTEGER, autoIncrement:true, allowNull:false,unique:true},
-    nome: {type: DataTypes.STRING, allowNull:false},
+    name: {type: DataTypes.STRING, allowNull:false},
     email:{type:DataTypes.STRING, allowNull:false, unique:true},
-    telefone:{type:DataTypes.STRING, allowNull:false},
-    password:{type:DataTypes.STRING, allowNull:false},
+    telephone:{type:DataTypes.STRING, allowNull:false},
 });
 
 module.exports = Applicant;
