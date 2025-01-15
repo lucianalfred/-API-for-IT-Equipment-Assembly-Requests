6711399b6e65d65adc5cbf6e5cdb79876b306f96
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 09:18:08 +0100

    Request model - applicant id added

--- src/models/Request.js
+++ src/models/Request.js
@@ -5,7 +5,9 @@ const Request =  sequelize.define('Request',{
     idRequest: {type:DataTypes.INTEGER, autoIncrement:true,allowNull:false},
     status: {type:DataTypes.BOOLEAN,allowNull:false, defaultValue:false},
     priority:{type:DataTypes.INTEGER,allowNull:false},
+    applicantId: {type:DatatTypes.INTEGER, allowNull:false},
     technicianId:{type:DatatTypes.INTEGER, allowNull:false},
 });
+
 
 module.exports = Request;
